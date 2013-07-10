895f3ea92e7ad51a0292f4e9b9250318ed06debd
Andrey <andrejev2006@gmail.com>
2013-07-10 19:52:21 +0000

    hw3 complete

--- grader.js
+++ grader.js
@@ -1,73 +1,117 @@
 #!/usr/bin/env node
 /*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
+ Automatically grade files for the presence of specified HTML tags/attributes.
+ Uses commander.js and cheerio. Teaches command line application development
+ and basic DOM parsing.
 
-References:
+ References:
 
  + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
+ - https://github.com/MatthewMueller/cheerio
+ - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+ - http://maxogden.com/scraping-with-node.html
 
  + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+ - https://github.com/visionmedia/commander.js
+ - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
 
  + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-*/
+ - http://en.wikipedia.org/wiki/JSON
+ - https://developer.mozilla.org/en-US/docs/JSON
+ - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+ */
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://aqueous-caverns-1182.herokuapp.com/";
 
-var assertFileExists = function(infile) {
-	var instr = infile.toString();
-	if(!fs.existsSync(instr)) {
-		console.log("%s does not exist. Exiting.", instr);
-		process.exit(1);
-	}
-	return instr;
+var assertFileExists = function (infile) {
+    var instr = infile.toString();
+
+    if (!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1);
+    }
+    return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-	return cheerio.load(fs.readFileSync(htmlfile)); 
-};
+var assertUrlValid = function (url) {
+    var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
 
-var loadChecks = function(checksfile) {
-	return JSON.parse(fs.readFileSync(checksfile));
+    if (!regexp.test(url)) {
+        console.log("%s has invalid format. Exiting.", url);
+        process.exit(1);
+    }
+
+    return url;
 }
 
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-	$ = cheerioHtmlFile(htmlfile);
-	var checks = loadChecks(checksfile).sort();
-	var out = {};
-	for (var ii in checks) {
-		var present = $(checks[ii]).length > 0;
-		out[checks[ii]] = present;
-	}
-	return out;
+var cheerioHtmlFile = function (htmlfile) {
+    return cheerio.load(fs.readFileSync(htmlfile));
 };
 
-var clone = function(fn) {
-	return fn.bind({});
+var loadChecks = function (checksfile) {
+    return JSON.parse(fs.readFileSync(checksfile));
 };
 
-if (require.main == module) {
+
+var checkHtmlFile = function (htmlfile, checksfile) {
+    $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for (var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
+var checkUrlFile = function (url, checksfile) {
+
+    var out = {};
+    var checks = loadChecks(checksfile).sort();
+    restCall = rest.get(url).on('complete', function (data) {
+        if (data instanceof Error) {
+            console.log("Error occured: %s.", data.message);
+            process.exit(1);
+        } else {
+            for (var ii in checks) {
+                var present = data(checks[ii]).length > 0;
+                out[checks[ii]] = present;
+            }
+        }
+    });
+
+    return out;
+};
+
+
+var clone = function (fn) {
+    return fn.bind({});
+};
+
+
+if (require.main === module) {
+
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Web address', clone(assertUrlValid), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var checkJson;
+    if (program.file) {
+        checkJson = checkHtmlFile(program.file, program.checks);
+    } else {
+        checkJson = checkUrlFile(program.url, program.checks);
+    }
+
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
-	exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtmlFile = checkHtmlFile;
 }
